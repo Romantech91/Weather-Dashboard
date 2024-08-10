908d6a4ddc5dcbdea9229462246aecb2be905e42
Victor Roman <Romantech91>
2024-08-09 22:47:43 -0700

    server.ts file configured

--- Develop/server/src/server.ts
+++ Develop/server/src/server.ts
@@ -8,14 +8,15 @@ import routes from './routes/index.js';
 const app = express();
 
 const PORT = process.env.PORT || 3001;
-
+app.use(express.static('client/dist'));
 
 // TODO: Serve static files of entire client dist folder
-
+app.use(express.json());
 // TODO: Implement middleware for parsing JSON and urlencoded form data
-
+app.use(express.urlencoded({ extended: true }));
 // TODO: Implement middleware to connect the routes
-app.use(routes);
+app.use(routes.);
+
 
 // Start the server on the port
 app.listen(PORT, () => console.log(`Listening on PORT: ${PORT}`));
