da873fe8a3fe5417171c45383a7c20084f99b331
Victor Roman <Romantech91>
2024-08-18 19:15:09 -0700

    all files proof read and edited before read me file updated.

--- Develop/server/src/server.ts
+++ Develop/server/src/server.ts
@@ -15,7 +15,7 @@ app.use(express.json());
 // TODO: Implement middleware for parsing JSON and urlencoded form data
 app.use(express.urlencoded({ extended: true }));
 // TODO: Implement middleware to connect the routes
-app.use(routes.);
+app.use(routes);
 
 
 // Start the server on the port
